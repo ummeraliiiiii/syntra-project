5e9f81f1546866da877381d6bf8a427951801240
mufeethak786-cell <mufeetha.k786@gmail.com>
2026-07-10 15:07:07 +0530

    debug

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -100,9 +100,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if os.environ.get("DATABASE_URL"):
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
     DATABASES = {
-        "default": dj_database_url.config(
+        "default": dj_database_url.parse(
+            DATABASE_URL,
             conn_max_age=600,
             ssl_require=False,
         )
